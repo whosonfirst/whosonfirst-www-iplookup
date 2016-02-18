371727a90437ebd0cd7fbabb955aed8cbd14075e
OpsWorks user acope <acope@iplookup-1.localdomain>
2016-02-18 21:37:37 +0000

    make setup wrangling

---
+++ ubuntu/setup-iplookup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PYTHON=`which python`
+WHOAMI=`${PYTHON} -c 'import os, sys; print os.path.realpath(sys.argv[1])' $0`
+
+PARENT=`dirname ${WHOAMI}`
+PROJECT=`dirname ${PARENT}`
+
+UBUNTU="${PROJECT}/ubuntu"
+SERVICES="${PROJECT}/services"
+
+${UBUNTU}/setup-golang.sh
+
+if [ ! -d /usr/local/mapzen/go-whosonfirst-iplookup ]
+then
+    git clone git@github.com:whosonfirst/go-whosonfirst-iplookup.git /usr/local/mapzen/go-whosonfirst-iplookup
+fi
+
+cd /usr/local/mapzen/go-whosonfirst-iplookup
+git pull origin master
+make build
+
+cd - 
+
+cp /usr/local/mapzen/go-whosonfirst-iplookup/bin/wof-iplookup-server ${SERVICES}/iplookup-server/wof-iplookup-server
--- ubuntu/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo apt-get update
-sudo apt-get upgrate -y
-
-sudo apt-get install nginx nginx-extras
