8439590f46c9e9e08a9c94862689851efab0cdce
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2016-02-18 17:39:25 -0800

    Update setup-iplookup.sh

--- ubuntu/setup-iplookup.sh
+++ ubuntu/setup-iplookup.sh
@@ -11,6 +11,10 @@ SERVICES="${PROJECT}/services"
 
 ${UBUNTU}/setup-golang.sh
 
+# TO DO: check (assume?) whether is running from a headless account and fetch
+# from https://github... and don't bother pulling from master
+# (20160218/thisisaaronland)
+
 if [ ! -d /usr/local/mapzen/go-whosonfirst-iplookup ]
 then
     git clone git@github.com:whosonfirst/go-whosonfirst-iplookup.git /usr/local/mapzen/go-whosonfirst-iplookup
